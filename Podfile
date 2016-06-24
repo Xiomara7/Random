12b2d617a5155cd3d2fc4b945f3049476ca9f4aa
Xiomara <xiomara@Xiomaras-MacBook-Pro.local>
2016-06-23 21:47:08 -0400

    Created podfile with dependencies (PureLayout & AFNetworking)

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+
+target 'Random' do
+
+pod 'PureLayout', '~> 2.0.1'
+pod 'AFNetworking', '~> 3.0'
+
+end
---
+++ Random/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Random
+//
+//  Created by Xiomara on 6/23/16.
+//  Copyright © 2016 Xiomara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
